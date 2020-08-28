ba47e20c34379dc3ccbe9a6f954e8ec2c0bab354
Shootzz <cannburak9@gmail.com>
2020-08-28 13:17:42 +0300

    Add files via upload

---
+++ corona.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+vaka=$(curl -s https://covid19.saglik.gov.tr/ | grep -A2 -i 'bugünkü<br>Ha' | grep '">' | cut -d "<" -f2 | cut -d ">" -f2)
+
+gun=$(curl -s https://covid19.saglik.gov.tr/ | grep -A1 -i 'takvim' | grep '</' | cut -d ">" -f2 | cut -d "<" -f1)
+
+test_sayi=$(curl -s https://covid19.saglik.gov.tr/ | grep  "buyuk-bilgi-l-sayi" | cut -d ">" -f2 | cut -d "<" -f1)
+
+iyilesen=$(curl -s https://covid19.saglik.gov.tr/ | grep -A2 "BUGÜNKÜ<br>İYİLEŞEN" | grep '">' | cut -d "<" -f2 | cut -d ">" -f2)
+
+printf -v ay "%(%B)T"
+
+printf " Vaka sayısı : $vaka\n Tarih       : $gun $ay\n Test sayısı : $test_sayi\n İyileşen sayısı : $iyilesen\n"
+
+cat vakalar.txt &> /dev/null
+if [ $? -ne 0 ] ; then
+	printf "\n" > vakalar.txt
+fi
+
+grep "$gun $ay" vakalar.txt &> /dev/null
+if [ $? -ne 0 ] ; then
+	sed -i "1i[-] $gun $ay       : $vaka\n    Test sayısı     : $test_sayi\n    İyileşen sayısı : $iyilesen\n" vakalar.txt
+else
+	exit 1
+fi
---
+++ korona.bat
@@ -0,0 +1,18 @@
+@echo off
+color 0E
+echo   _____                               _                            _                     
+echo  / ____^|                             (_)                          (_)                    
+echo ^| ^|     ___  _ __ ___  _ __   __ _    _ ___     ___ ___  _ __ ___  _ _ __   __ _         
+echo ^| ^|    / _ \^| '__/ _ \^| '_ \ / _` ^|  ^| / __^|   / __/ _ \^| '_ ` _ \^| ^| '_ \ / _` ^|        
+echo ^| ^|___^| (_) ^| ^| ^| (_) ^| ^| ^| ^| (_^| ^|  ^| \__ \  ^| (_^| (_) ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| (_^| ^|  _ _ _ 
+echo  \_____\___/^|_^|  \___/^|_^| ^|_^|\__,_^|  ^|_^|___/   \___\___/^|_^| ^|_^| ^|_^|_^|_^| ^|_^|\__, ^| (_^|_^|_)
+echo                                                                             __/ ^|        
+echo                                                                            ^|___/    
+bash -c ./corona.sh
+echo.
+echo Vakalar.txt has been created to store the information. 
+echo.
+echo   ---- Press any key to close the terminal ----
+:End
+pause >nul
+
